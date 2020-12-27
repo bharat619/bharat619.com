575eb7959d8ec5b09203f0ea306037037e878b7e
bharat619 <bharat.h@kiprosh.com>
2020-12-27 13:07:12 +0530

    Add logs

--- functions/instagram/instagram.js
+++ functions/instagram/instagram.js
@@ -8,6 +8,7 @@ exports.handler = async (event, context) => {
   const query = `https://www.instagram.com/graphql/query/?query_hash=472f257a40c653c64c666ce877d59d2b&variables={"id":"2157021316","first":8, "after":"QVFDVURvcXZYNFNuRnhTUlJQNlkzR0VTNDFTdHhCc0xzOXd4MzhBR3ZZZTFlU3VmOWlUM2JsaVVhVlk1Wm40SUpuc2V4aGFGVkw3d3BGVS1oVVlEekNwTQ=="}`;
   console.log(cachedPosts.posts);
   if (cachedPosts.posts.length) {
+    console.log(`returning cached ${igPosts}, ${JSON.stringify(igPosts)}`);
     return {
       statusCode: 200,
       body: JSON.stringify(cachedPosts.post),
@@ -25,6 +26,9 @@ exports.handler = async (event, context) => {
     );
 
     cachedPosts.posts = igPosts;
+    console.log(
+      `fetching and returning ${igPosts}, ${JSON.stringify(igPosts)}`
+    );
     return {
       statusCode: 200,
       body: JSON.stringify(igPosts),
