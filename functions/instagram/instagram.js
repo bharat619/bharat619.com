140a6614120e65f57ecba3fc7f4b14cd56f78d8b
bharat619 <bharat.h@kiprosh.com>
2021-01-05 00:03:05 +0530

    Use callback

--- functions/instagram/instagram.js
+++ functions/instagram/instagram.js
@@ -1,26 +1,17 @@
 require("isomorphic-fetch");
 
 const cachedPosts = {
-  posts: [],
+  posts: null,
 };
 
-exports.handler = async (event, context) => {
+exports.handler = async (event, context, callback) => {
   const query = `https://www.instagram.com/graphql/query/?query_hash=472f257a40c653c64c666ce877d59d2b&variables={"id":"2157021316","first":8, "after":"QVFDVURvcXZYNFNuRnhTUlJQNlkzR0VTNDFTdHhCc0xzOXd4MzhBR3ZZZTFlU3VmOWlUM2JsaVVhVlk1Wm40SUpuc2V4aGFGVkw3d3BGVS1oVVlEekNwTQ=="}`;
-  console.log(cachedPosts.posts);
-  if (cachedPosts.posts.length) {
-    console.log(
-      `returning cached ${cachedPosts.posts}, ${JSON.stringify(
-        cachedPosts.posts
-      )}`
-    );
-    return {
-      statusCode: 200,
-      body: JSON.stringify(cachedPosts.post),
-    };
+  let igPosts = null;
+  if (cachedPosts.posts) {
+    igPosts = cachedPosts.posts;
   } else {
     const response = await fetch(query).then((res) => res.json());
-    console.log(response);
-    const igPosts = response.data.user.edge_owner_to_timeline_media.edges.map(
+    igPosts = response.data.user.edge_owner_to_timeline_media.edges.map(
       (edge) => ({
         id: edge.node.id,
         thumbnail: edge.node.thumbnail_resources[4].src,
@@ -30,12 +21,13 @@ exports.handler = async (event, context) => {
     );
 
     cachedPosts.posts = igPosts;
-    console.log(
-      `fetching and returning ${igPosts}, ${JSON.stringify(igPosts)}`
-    );
-    return {
+    console.log(igPosts);
+    callback(null, {
       statusCode: 200,
+      headers: {
+        "Content-Type": "application/json",
+      },
       body: JSON.stringify(igPosts),
-    };
+    });
   }
 };
