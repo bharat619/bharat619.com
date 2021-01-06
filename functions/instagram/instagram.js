b00bd57a81f9395fe73787a57da70402541d56c6
bharat619 <bharat.h@kiprosh.com>
2021-01-06 12:21:14 +0530

    Add caching of IG posts

--- functions/instagram/instagram.js
+++ functions/instagram/instagram.js
@@ -1,24 +1,36 @@
 require("isomorphic-fetch");
 
-exports.handler = async (event, context, callback) => {
-  const query = `https://www.instagram.com/graphql/query/?query_hash=472f257a40c653c64c666ce877d59d2b&variables={"id":"2157021316","first":8, "after":"QVFDVURvcXZYNFNuRnhTUlJQNlkzR0VTNDFTdHhCc0xzOXd4MzhBR3ZZZTFlU3VmOWlUM2JsaVVhVlk1Wm40SUpuc2V4aGFGVkw3d3BGVS1oVVlEekNwTQ=="}`;
+const cached_data = {
+  posts: null,
+};
+
+const query = `https://www.instagram.com/graphql/query/?query_hash=472f257a40c653c64c666ce877d59d2b&variables={"id":"2157021316","first":8, "after":"QVFDVURvcXZYNFNuRnhTUlJQNlkzR0VTNDFTdHhCc0xzOXd4MzhBR3ZZZTFlU3VmOWlUM2JsaVVhVlk1Wm40SUpuc2V4aGFGVkw3d3BGVS1oVVlEekNwTQ=="}`;
+
+const loadIgPosts = async () => {
   let igPosts = null;
+  if (!cached_data.posts) {
+    const response = await fetch(query).then((res) => res.json());
+    igPosts = response.data.user.edge_owner_to_timeline_media.edges.map(
+      (edge) => ({
+        id: edge.node.id,
+        thumbnail: edge.node.thumbnail_resources[4].src,
+        url: `https://instagram.com/p/${edge.node.shortcode}`,
+        alt: edge.node.text,
+      })
+    );
+    console.log(`Caching ${igPosts}`);
+    cached_data.posts = igPosts;
+  }
+  console.log(`returning ${cached_data.posts}`);
+  return cached_data.posts;
+};
 
-  const response = await fetch(query).then((res) => res.json());
-  igPosts = response.data.user.edge_owner_to_timeline_media.edges.map(
-    (edge) => ({
-      id: edge.node.id,
-      thumbnail: edge.node.thumbnail_resources[4].src,
-      url: `https://instagram.com/p/${edge.node.shortcode}`,
-      alt: edge.node.text,
-    })
-  );
-
-  callback(null, {
+exports.handler = async (event, context, callback) => {
+  return {
     statusCode: 200,
     headers: {
       "Content-Type": "application/json",
     },
-    body: JSON.stringify(igPosts),
-  });
+    body: JSON.stringify(await loadIgPosts()),
+  };
 };
